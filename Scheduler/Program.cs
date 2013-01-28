8d1c4cda1e380682b87f83e2f9c5e66cc95e1f4e
Alden Quimby <aldenquimby@gmail.com>
2013-01-28 10:33:49 -0500

    remove nunit

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -6,10 +6,9 @@ namespace Columbia.Scheduler
     {
         public static void Main(string[] args)
         {
-            var callNumber = GetCallNumber(args);
-
-            const string uName = "usernameHere";
-            const string pWord = "passwordHere";
+            const string callNumber = "16338";
+            const string uName = "usernamehere";
+            const string pWord = "passwordhere";
 
             try
             {
@@ -22,37 +21,6 @@ namespace Columbia.Scheduler
 
             Console.Write("Done!");
             Console.ReadKey();
-        }
-
-        private static string GetCallNumber(string[] args)
-        {
-            var callNumber = "78696";
-
-            if (args.Length > 0)
-            {
-                var type = (ResponseType)Enum.Parse(typeof(ResponseType), args[0]);
-
-                switch (type)
-                {
-                    case ResponseType.Blocked:
-                        callNumber = "72208";
-                        break;
-                    case ResponseType.Conflict:
-                        callNumber = "dontknow";
-                        break;
-                    case ResponseType.Open:
-                        callNumber = "05362";
-                        break;
-                    case ResponseType.Restricted:
-                        callNumber = "16598";
-                        break;
-                    case ResponseType.Registered:
-                        callNumber = "78696";
-                        break;
-                }
-            }
-
-            return callNumber;
         }
     }
 }
